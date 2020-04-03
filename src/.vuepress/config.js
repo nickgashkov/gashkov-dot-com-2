f45b01eca0753c3cb0a66b22acc1ea94d68439f1
Nick Gashkov <nick@gashkov.com>
2020-04-03 14:02:07 +0500

    Add CV's content (#3)
    * Add 'build' dependency for the 'run' rule
    * Add CV's content
    * Add static files
    * Update website configuration

---
+++ src/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  description: "Nick Gashkov's CV of some sort",
+  lang: "en-USΩ",
+  themeConfig: {
+    nav: [
+      {
+        text: "LinkedIn",
+        link: "https://linkedin.com/in/gashkov",
+        target:"_blank",
+        rel:""
+      },
+      {
+        text: "Github",
+        link: "https://github.com/nickgashkov",
+        target:"_blank",
+        rel:""
+      },
+    ],
+    repo: "nickgashkov/gashkov-dot-com-2",
+    repoLabel: "Source",
+  }
+};
